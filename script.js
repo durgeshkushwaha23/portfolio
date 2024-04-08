30234c6ba69d27262ca81854918234ec6355682b
Durgesh Kushwaha <durgeshkushwaha415@gmail.com>
2024-04-08 15:28:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,21 @@
 
 
 
+var locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".smooth-scroll"),
+  smooth: true,
+  mobile: {
+     smooth: true
+ },
+ tablet: {
+     smooth: true
+ }
 
+});
+var locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".smooth-scroll"),
+  smooth: true
+});
 
 
 
